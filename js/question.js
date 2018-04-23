6d16ac47ca0a55603708ebcf3d9ef241c5596833
MARIO NISHIO <mario.nishio@gmail.com>
2018-04-23 16:47:26 -0700

    hooked up with the submit button

--- js/question.js
+++ js/question.js
@@ -8,13 +8,17 @@ h2El.textContent = Question.allQuestions[0].text;
 var labelEl = document.getElementsByTagName('label')[1];
 labelEl.textContent = Question.allQuestions[0].optionC.text;
 
-var labelEl = document.getElementsByTagName('label')[2];
+labelEl = document.getElementsByTagName('label')[2];
 labelEl.textContent = Question.allQuestions[0].optionJ.text;
-var labelEl = document.getElementsByTagName('label')[3];
+labelEl = document.getElementsByTagName('label')[3];
 labelEl.textContent = Question.allQuestions[0].optionP.text;
-var labelEl = document.getElementsByTagName('label')[4];
+labelEl = document.getElementsByTagName('label')[4];
 labelEl.textContent = Question.allQuestions[0].optionS.text;
 
 function handleSubmit(event) {
   event.preventDefault();
+  console.log('yo');
 }
+
+var formEl = document.getElementById('mainQuiz');
+formEl.addEventListener('submit', handleSubmit);
