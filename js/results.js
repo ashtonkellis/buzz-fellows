5e713cb4215b7a89e66d7d88e9371cd1d9863c96
Ashton K Ellis <ashtonkellis@gmail.com>
2018-04-24 16:48:33 -0700

    render results looping through all users

--- js/results.js
+++ js/results.js
@@ -41,13 +41,36 @@ function calculateChartData() {
   chartData.datasets[0].backgroundColor.push('rgba(58, 123, 193, 1)');
   chartData.datasets[0].backgroundColor.push('rgba(244, 235, 66, 1)');
   // all courses chart - border colors
-  chartData.datasets[0].backgroundColor.push('rgba(102, 51, 153, 1.0)');
-  chartData.datasets[0].backgroundColor.push('rgba(209, 31, 31, 1.0)');
-  chartData.datasets[0].backgroundColor.push('rgba(58, 123, 193, 1.0)');
-  chartData.datasets[0].backgroundColor.push('rgba(244, 235, 66, 1.0)');
+  chartData.datasets[0].backgroundColor.push('rgba(102, 51, 153, 1)');
+  chartData.datasets[0].backgroundColor.push('rgba(209, 31, 31, 1)');
+  chartData.datasets[0].backgroundColor.push('rgba(58, 123, 193, 1)');
+  chartData.datasets[0].backgroundColor.push('rgba(244, 235, 66, 1)');
 
   var ctx = document.getElementById('user-results-chart').getContext('2d');
   var myChart = new Chart(ctx, currentUser.allChartData);
 }
 
-var test = calculateChartData();
+function renderAllResults () {
+  var allResultsSection = document.getElementById('all-results-section');
+  for (var user of User.allUser) {
+    // create user-card div
+    var divEL = document.createElement('div');
+    divEL.class = 'user-card';
+    // create h3 element
+    var h3EL = document.createElement('h3');
+    h3EL.textContent = user.username;
+    // create canvas element
+    var canvasEL = document.createElement('canvas');
+    var canvasId = user.username + '-results-chart';
+    canvasEL.id = canvasId;
+    // append elements to div
+    divEL.appendChild(h3EL);
+    divEL.appendChild(canvasEL);
+    // append div to all-results section
+    allResultsSection.appendChild(divEL);
+  }
+
+}
+
+renderAllResults();
+calculateChartData();
